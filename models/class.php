1f4f33379d5c8a71650c83237b60831a830164d2
arhen <official.rahmatslamet@gmail.com>
2016-02-17 19:27:38 +0800

    Tambahkan File Pertama

---
+++ models/class.php
@@ -0,0 +1,21 @@
+<?php 
+	ob_start();
+	require_once("connect/database.php");
+	function autoload($class){
+		$namafile = $class.'.php';
+		include_once $namafile;
+	}
+
+	spl_autoload_register('autoload');
+
+	try {
+		$pagemodel		= new pagemodel_model($db);
+		$input			= new input_model($db);
+		$input_tanah	= new input_tanah_model ($db);
+		$libs			= new libs_model($db);
+		$user			= new user_model($db);
+
+	} catch(Exception $e) {
+		echo "Menemukan kesalahan: ".$e->getMessage()."\n";
+	}
+?>
